38c77d8a9718355fbde5e815769d554df1ffb5ae
billstark_git <18910855655@163.com>
2025-05-04 20:27:24 +0800

    依赖初步修改

---
+++ common/src/test/java/xyz/tjucomments/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tjucomments.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
