b97098890bed3468875622381f862b961e1ba841
billstark_git <18910855655@163.com>
2025-05-04 20:25:25 +0800

    依赖初步修改

---
+++ src/main/java/xyz/tjucomments/tjucommentv1/TjucommentV1Application.java
@@ -0,0 +1,13 @@
+package xyz.tjucomments.tjucommentv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjucommentV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjucommentV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/tjucomments/tjucommentv1/TjucommentV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tjucomments.tjucommentv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjucommentV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
