a95117c3e6c068de4bad649eb74370b62846c5ea
billstark_git <18910855655@163.com>
2025-05-05 09:50:44 +0800

    添加了一些文件

---
+++ common/src/main/java/xyz/tjucomments/common/constant/AutoFillConstant.java
@@ -0,0 +1,35 @@
+package xyz.tjucomments.common.constant;
+
+/**
+ * 自动填充相关常量
+ * 
+ * @author 赵嘉宁
+ * @since 2025-05-04
+ */
+public class AutoFillConstant {
+    
+    /**
+     * 创建时间
+     */
+    public static final String CREATE_TIME = "createTime";
+    
+    /**
+     * 创建人
+     */
+    public static final String CREATE_USER = "createUser";
+    
+    /**
+     * 更新时间
+     */
+    public static final String UPDATE_TIME = "updateTime";
+    
+    /**
+     * 更新人
+     */
+    public static final String UPDATE_USER = "updateUser";
+    
+    /**
+     * 是否删除（逻辑删除）
+     */
+    public static final String IS_DELETED = "isDeleted";
+}
