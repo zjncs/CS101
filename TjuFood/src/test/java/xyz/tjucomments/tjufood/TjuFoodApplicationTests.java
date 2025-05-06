5740a6d0b3e905c9dec10d098f30bbbbf26ffb7a
billstark_git <18910855655@163.com>
2025-05-06 17:58:42 +0800

    创建项目

---
+++ TjuFood/src/test/java/xyz/tjucomments/tjufood/TjuFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tjucomments.tjufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjuFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
