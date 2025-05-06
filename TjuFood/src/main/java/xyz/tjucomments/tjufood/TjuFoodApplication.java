0d66b59b09c77761a3846819f2a078f130ca7c4e
billstark_git <18910855655@163.com>
2025-05-06 17:58:42 +0800

    创建项目

---
+++ TjuFood/src/main/java/xyz/tjucomments/tjufood/TjuFoodApplication.java
@@ -0,0 +1,13 @@
+package xyz.tjucomments.tjufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjuFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjuFoodApplication.class, args);
+    }
+
+}
